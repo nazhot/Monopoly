a75605a6c30a471d25d5ebb1aed9c12bbafe9bcf
nazhot <15216154+nazhot@users.noreply.github.com>
2024-01-25 15:50:30 -0700

    Added Tile Header

---
+++ src/tile.c
@@ -0,0 +1,3 @@
+#include "tile.h"
+#include <stdio.h>
+#include <stdlib.h>
---
+++ src/tile.h
@@ -0,0 +1,31 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef unsigned int money;
+
+enum PropType {
+    PROPERTY,
+    UTILITY,
+    RAILROAD,
+    FREE_PARKING,
+    JAIL,
+    GO_TO_JAIL,
+    COMMUNITY_CHEST,
+    CHANCE,
+    GO,
+    TAX
+};
+
+struct Tile {
+    char *name;
+    money cost;
+    money mortgageValue;
+    enum PropType type;
+    uint8_t group;
+    bool isMortgaged;
+};
+
+#endif
